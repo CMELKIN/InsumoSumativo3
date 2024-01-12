9888f8a4f720db2b506d76915fb8aa57b30a4797
CMELKIN <elkin.chambam.est@uets.edu.ec>
2024-01-12 07:34:08 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'InsumoSumativo3',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
